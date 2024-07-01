c47a8e046b02b7d32c310089768e5218f148b08e
YA.Kim <170071016+Young-Ah-Kim@users.noreply.github.com>
2024-07-01 13:51:11 +0900

    Create 12941(최솟값 만들기).js

---
+++ JS/12941(최솟값 만들기).js
@@ -0,0 +1,14 @@
+function solution(A, B) {
+
+    let count = 0;
+
+    A.sort((a, b) => (a-b));
+    B.sort((a, b) => (b-a));
+
+    for (let i = 0; i<A.length; i++){
+        count += A[i] * B[i];
+    }
+
+    return count;
+
+}

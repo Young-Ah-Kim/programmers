85faf005fb8d6f61ec952147381637f06ee1314f
YA.Kim <170071016+Young-Ah-Kim@users.noreply.github.com>
2024-08-09 23:57:19 +0900

    Create 133502(햄버거 만들기).py

---
+++ 133502(햄버거 만들기).py
@@ -0,0 +1,9 @@
+def solution(ingredient):
+    stack = []
+    count = 0
+    for n in ingredient:
+        stack.append(n)
+        if stack[-4:] == [1,2,3,1]:
+            count += 1
+            del(stack[-4:])
+    return count
